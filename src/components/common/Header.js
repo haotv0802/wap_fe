e18603e944daa687535467d1f06d937b019113b6
Hao Ho ^ <haotv0802@gmail.com>
2017-12-01 13:30:04 +0700

    Add loading dots.

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -4,28 +4,28 @@ import LoadingDots from './LoadingDots';
 
 const Header = ({loading}) => {
   return (
-    <div className="container-fluid" style={{width: '800px'}}>
+    <div className="container-fluid" style={{width: '850px'}}>
       <div className="row" >
         <div className="col-md-2">
           <img src={require("../../assets/images/logo.png")}
                className="img-responsive center-block"
                style={{maxHeight: '54px', maxWidth: '188px'}}/>
         </div>
-        <div className="col-md-8" style={{textAlign: 'right'}}>
+        <div className="col-md-10" style={{textAlign: 'left', textIndent: '150px'}}>
           <nav>
             <IndexLink to="/" activeClassName="active"/>
-            &nbsp;&nbsp;&nbsp;&nbsp;
+            &nbsp;&nbsp;&nbsp;
             <Link to="/businessLoans" activeClassName="active">Business Loans    </Link>
-            &nbsp;&nbsp;&nbsp;&nbsp;{"  |  "}&nbsp;&nbsp;&nbsp;&nbsp;
+            &nbsp;&nbsp;&nbsp;{"  |  "}&nbsp;&nbsp;&nbsp;
             <Link to="/howItWorks" activeClassName="active">How it works    </Link>
-            &nbsp;&nbsp;&nbsp;&nbsp;{"  |  "}&nbsp;&nbsp;&nbsp;&nbsp;
+            &nbsp;&nbsp;&nbsp;{"  |  "}&nbsp;&nbsp;&nbsp;
             <Link to="/useCases" activeClassName="active">Use Cases    </Link>
-            &nbsp;&nbsp;&nbsp;&nbsp;{"  |  "}&nbsp;&nbsp;&nbsp;&nbsp;
+            &nbsp;&nbsp;&nbsp;{"  |  "}&nbsp;&nbsp;&nbsp;
             <Link to="/whyAspire" activeClassName="active">Why Aspire</Link>
+            &nbsp;&nbsp;&nbsp;<LoadingDots interval={100} dots={10}/>
           </nav>
         </div>
       </div>
-      {loading && <LoadingDots interval={100} dots={10}/>}
       <div style={{border: '1px solid black'}} />
     </div>
   );
@@ -33,7 +33,6 @@ const Header = ({loading}) => {
 
 
 Header.propTypes = {
-  loading: PropTypes.boolean.isRequired
 };
 
 export default Header;
