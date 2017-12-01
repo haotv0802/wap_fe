ac11ff1b4ad59cfa9df0be73d4e024abb9ac096a
Hao Ho ^ <haotv0802@gmail.com>
2017-12-01 11:41:44 +0700

    Add solid line to Header.

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -4,18 +4,29 @@ import LoadingDots from './LoadingDots';
 
 const Header = ({loading}) => {
   return (
-    <nav style={{textAlign: 'right'}}>
-      <IndexLink to="/" activeClassName="active">Aspire</IndexLink>
-      &nbsp;&nbsp;&nbsp;&nbsp;
-      <Link to="/businessLoans" activeClassName="active">Business Loans    </Link>
-      &nbsp;&nbsp;&nbsp;&nbsp;{"  |  "}&nbsp;&nbsp;&nbsp;&nbsp;
-      <Link to="/howItWorks" activeClassName="active">How it works    </Link>
-      &nbsp;&nbsp;&nbsp;&nbsp;{"  |  "}&nbsp;&nbsp;&nbsp;&nbsp;
-      <Link to="/useCases" activeClassName="active">Use Cases    </Link>
-      &nbsp;&nbsp;&nbsp;&nbsp;{"  |  "}&nbsp;&nbsp;&nbsp;&nbsp;
-      <Link to="/whyAspire" activeClassName="active">Why Aspire</Link>
-      {loading && <LoadingDots interval={100} dots={10}/>}
-    </nav>
+    <div className="container-fluid" style={{width: '800px'}}>
+      <div className="row" >
+        <div className="col-md-2">
+          <img src={require("../../assets/images/logo.png")}
+               className="img-responsive center-block"
+               style={{maxHeight: '54px', maxWidth: '188px'}}/>
+        </div>
+        <div className="col-md-8" style={{textAlign: 'right'}}>
+          <nav>
+            <IndexLink to="/" activeClassName="active">  </IndexLink>
+            &nbsp;&nbsp;&nbsp;&nbsp;
+            <Link to="/businessLoans" activeClassName="active">Business Loans    </Link>
+            &nbsp;&nbsp;&nbsp;&nbsp;{"  |  "}&nbsp;&nbsp;&nbsp;&nbsp;
+            <Link to="/howItWorks" activeClassName="active">How it works    </Link>
+            &nbsp;&nbsp;&nbsp;&nbsp;{"  |  "}&nbsp;&nbsp;&nbsp;&nbsp;
+            <Link to="/useCases" activeClassName="active">Use Cases    </Link>
+            &nbsp;&nbsp;&nbsp;&nbsp;{"  |  "}&nbsp;&nbsp;&nbsp;&nbsp;
+            <Link to="/whyAspire" activeClassName="active">Why Aspire</Link>
+          </nav>
+        </div>
+      </div>
+      <div style={{border: '1px solid black'}} />
+    </div>
   );
 };
 
