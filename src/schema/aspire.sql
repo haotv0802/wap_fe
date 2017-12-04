79b15522f57a7741c184cc89c01f9f1209a5ee3d
Hao Ho ^ <haotv0802@gmail.com>
2017-12-04 07:32:36 +0700

    Update sql with Decimal(13,4)

--- src/schema/aspire.sql
+++ src/schema/aspire.sql
@@ -7,16 +7,19 @@ USE `aspire_db`;
 --
 DROP TABLE IF EXISTS `aspire_users`;
 CREATE TABLE `aspire_users` (
-  `id`            BIGINT   AUTO_INCREMENT,
-  `email`         VARCHAR(30) NULL,
-  `password`      VARCHAR(30) NULL,
-  `referral_code` VARCHAR(30) NULL,
-  `referred_by`   VARCHAR(30) NULL,
-  `credit_score`  TINYINT     NULL,
-  `created_on`    DATETIME DEFAULT NOW(),
-  `updated_on`    DATETIME    NULL,
+  `id`                BIGINT   AUTO_INCREMENT,
+  `email`             VARCHAR(30)  NULL,
+  `password`          VARCHAR(255) NULL,
+  `referral_code`     VARCHAR(30)  NULL,
+  `referred_by`       VARCHAR(30)  NULL,
+  `credit_score`      TINYINT      NULL,
+  `email_verified_at` DATETIME     NULL,
+  `phone_verified_at` DATETIME     NULL,
+  `created_on`        DATETIME DEFAULT NOW(),
+  `updated_on`        DATETIME     NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `aspire_users_id` (`id`)
+  #   CONSTRAINT `aspire_user_referred_by` FOREIGN KEY (`referred_by`) REFERENCES `aspire_users` (`referral_code`)
   #   CONSTRAINT `aspire_user_referred_by` FOREIGN KEY (`referred_by`) REFERENCES `aspire_users` (`referral_code`)
 )
   ENGINE = INNODB
@@ -30,16 +33,18 @@ INSERT INTO aspire_users (`email`, `password`, `referral_code`, `referred_by`, `
 --
 DROP TABLE IF EXISTS `aspire_business_info`;
 CREATE TABLE `aspire_business_info` (
-  `id`               BIGINT AUTO_INCREMENT,
-  `acra_number`      VARCHAR(100) NULL,
-  `business_name`    VARCHAR(30)  NULL,
-  `business_address` VARCHAR(30)  NULL,
-  `city`             VARCHAR(30)  NULL,
-  `cap`              VARCHAR(30)  NULL,
-  `busines_phone`    VARCHAR(30)  NULL,
-  `data_established` VARCHAR(30)  NULL,
-  `type_of_entity`   VARCHAR(30)  NULL,
-  `user_id`          BIGINT       NOT NULL,
+  `id`                BIGINT AUTO_INCREMENT,
+  `acra_number`       VARCHAR(100) NULL,
+  `business_name`     VARCHAR(30)  NULL,
+  `business_address`  VARCHAR(30)  NULL,
+  `city`              VARCHAR(30)  NULL,
+  `cap`               VARCHAR(30)  NULL,
+  `busines_phone`     VARCHAR(30)  NULL,
+  `data_established`  VARCHAR(30)  NULL,
+  `type_of_entity`    VARCHAR(30)  NULL,
+  `email_verified_at` DATETIME     NULL,
+  `phone_verified_at` DATETIME     NULL,
+  `user_id`           BIGINT       NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `aspire_business_info_id` (`id`),
   CONSTRAINT `aspire_business_info_user_id` FOREIGN KEY (`user_id`) REFERENCES `aspire_users` (`id`)
@@ -52,16 +57,18 @@ CREATE TABLE `aspire_business_info` (
 --
 DROP TABLE IF EXISTS `aspire_personal_info`;
 CREATE TABLE `aspire_personal_info` (
-  `id`            BIGINT AUTO_INCREMENT,
-  `fin_number`    VARCHAR(100) NULL,
-  `first_name`    VARCHAR(30)  NULL,
-  `second_name`   VARCHAR(30)  NULL,
-  `address`       VARCHAR(30)  NULL,
-  `city`          VARCHAR(30)  NULL,
-  `cap`           VARCHAR(30)  NULL,
-  `phone`         VARCHAR(30)  NULL,
-  `date_of_birth` DATE         NULL,
-  `user_id`       BIGINT       NOT NULL,
+  `id`                BIGINT AUTO_INCREMENT,
+  `fin_number`        VARCHAR(100) NULL,
+  `first_name`        VARCHAR(30)  NULL,
+  `second_name`       VARCHAR(30)  NULL,
+  `address`           VARCHAR(30)  NULL,
+  `city`              VARCHAR(30)  NULL,
+  `cap`               VARCHAR(30)  NULL,
+  `phone`             VARCHAR(30)  NULL,
+  `date_of_birth`     DATE         NULL,
+  `email_verified_at` DATETIME     NULL,
+  `phone_verified_at` DATETIME     NULL,
+  `user_id`           BIGINT       NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `aspire_personal_info_id` (`id`),
   CONSTRAINT `aspire_personal_info_user_id` FOREIGN KEY (`user_id`) REFERENCES `aspire_users` (`id`)
@@ -69,19 +76,19 @@ CREATE TABLE `aspire_personal_info` (
   ENGINE = INNODB
   DEFAULT CHARSET = UTF8;
 
-DROP TABLE IF EXISTS `aspire_applications`;
-CREATE TABLE `aspire_applications` (
+DROP TABLE IF EXISTS `aspire_loans`;
+CREATE TABLE `aspire_loans` (
   `id`             BIGINT AUTO_INCREMENT,
-  `amount`         DECIMAL(9, 2) NULL,
-  `currency`       VARCHAR(30)   NULL,
-  `monthly_fee`    VARCHAR(30)   NULL,
-  `interest_rate`  VARCHAR(30)   NULL,
-  `payment_method` VARCHAR(30)   NULL,
-  `created_on`     DATETIME      NULL,
-  `user_id`        BIGINT        NOT NULL,
+  `amount`         DECIMAL(13, 4) NULL,
+  `currency`       VARCHAR(30)    NULL,
+  `monthly_fee`    VARCHAR(30)    NULL,
+  `interest_rate`  VARCHAR(30)    NULL,
+  `payment_method` VARCHAR(30)    NULL,
+  `created_on`     DATETIME       NULL,
+  `user_id`        BIGINT         NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `aspire_applications` (`id`),
-  CONSTRAINT `aspire_applications_user_id` FOREIGN KEY (`user_id`) REFERENCES `aspire_users` (`id`)
+  UNIQUE KEY `aspire_loans` (`id`),
+  CONSTRAINT `aspire_loans_user_id` FOREIGN KEY (`user_id`) REFERENCES `aspire_users` (`id`)
 )
   ENGINE = INNODB
   DEFAULT CHARSET = UTF8;
@@ -90,17 +97,17 @@ CREATE TABLE `aspire_applications` (
 DROP TABLE IF EXISTS `aspire_payments`;
 CREATE TABLE `aspire_payments` (
   `id`               BIGINT AUTO_INCREMENT,
-  `amount`           DECIMAL(9, 2) NULL,
-  `monthly_fee`      DECIMAL(9, 2) NULL,
-  `interest_rate`    DECIMAL(4, 2) NULL,
-  `payment_method`   VARCHAR(30)   NULL,
-  `fine`             DECIMAL(9, 2) NULL,
-  `fine_description` VARCHAR(30)   NULL,
-  `created_on`       DATETIME      NULL,
-  `application_id`   BIGINT        NOT NULL,
+  `amount`           DECIMAL(13, 4) NULL,
+  `monthly_fee`      DECIMAL(13, 4) NULL,
+  `interest_rate`    DECIMAL(4, 2)  NULL,
+  `payment_method`   VARCHAR(30)    NULL,
+  `fine`             DECIMAL(13, 4) NULL,
+  `fine_description` VARCHAR(30)    NULL,
+  `created_on`       DATETIME       NULL,
+  `load_id`   BIGINT         NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `aspire_payments` (`id`),
-  CONSTRAINT `aspire_payments_application_id` FOREIGN KEY (`application_id`) REFERENCES `aspire_applications` (`id`)
+  CONSTRAINT `aspire_payments_loan_id` FOREIGN KEY (`load_id`) REFERENCES `aspire_loans` (`id`)
 )
   ENGINE = INNODB
   DEFAULT CHARSET = UTF8;
@@ -108,13 +115,13 @@ CREATE TABLE `aspire_payments` (
 DROP TABLE IF EXISTS `aspire_credit_scores_history`;
 CREATE TABLE `aspire_credit_scores_history` (
   `id`          BIGINT  AUTO_INCREMENT,
-  `name`        DECIMAL(9, 2) NULL,
-  `source`      VARCHAR(30)   NULL,
-  `description` VARCHAR(30)   NULL,
+  `name`        VARCHAR(30) NULL,
+  `source`      VARCHAR(30) NULL,
+  `description` VARCHAR(30) NULL,
   `score`       TINYINT DEFAULT 1,
-  `created_on`  DATETIME      NULL,
-  `updated_on`  DATETIME      NULL,
-  `user_id`     BIGINT        NOT NULL,
+  `created_on`  DATETIME    NULL,
+  `updated_on`  DATETIME    NULL,
+  `user_id`     BIGINT      NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `aspire_credit_scores_history` (`id`),
   CONSTRAINT `aspire_credit_scores_history_user_id` FOREIGN KEY (`user_id`) REFERENCES `aspire_users` (`id`)
@@ -153,15 +160,15 @@ CREATE TABLE `aspire_lazada_sellers` (
 DROP TABLE IF EXISTS `aspire_lazada_transactions`;
 CREATE TABLE `aspire_lazada_transactions` (
   `id`         BIGINT AUTO_INCREMENT,
-  `name`       VARCHAR(100)  NULL,
-  `price`      DECIMAL(9, 2) NULL,
-  `currency`   VARCHAR(3)    NULL,
-  `category`   VARCHAR(50)   NULL,
-  `rating`     DECIMAL(4, 2) NULL,
-  `url`        TEXT          NULL,
-  `created_on` DATETIME      NULL,
-  `updated_on` DATETIME      NULL,
-  `seller_id`  BIGINT        NOT NULL,
+  `name`       VARCHAR(100)   NULL,
+  `price`      DECIMAL(13, 4) NULL,
+  `currency`   VARCHAR(3)     NULL,
+  `category`   VARCHAR(50)    NULL,
+  `rating`     DECIMAL(4, 2)  NULL,
+  `url`        TEXT           NULL,
+  `created_on` DATETIME       NULL,
+  `updated_on` DATETIME       NULL,
+  `seller_id`  BIGINT         NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `aspire_lazada_transactions` (`id`),
   CONSTRAINT `aspire_lazada_transactions_seller_id` FOREIGN KEY (`seller_id`) REFERENCES `aspire_lazada_sellers` (`id`)
@@ -195,15 +202,15 @@ CREATE TABLE `aspire_shopee_sellers` (
 DROP TABLE IF EXISTS `aspire_shopee_transactions`;
 CREATE TABLE `aspire_shopee_transactions` (
   `id`         BIGINT AUTO_INCREMENT,
-  `name`       VARCHAR(100)  NULL,
-  `price`      DECIMAL(9, 2) NULL,
-  `currency`   VARCHAR(3)    NULL,
-  `category`   VARCHAR(50)   NULL,
-  `rating`     DECIMAL(4, 2) NULL,
-  `url`        TEXT          NULL,
-  `created_on` DATETIME      NULL,
-  `updated_on` DATETIME      NULL,
-  `seller_id`  BIGINT        NOT NULL,
+  `name`       VARCHAR(100)   NULL,
+  `price`      DECIMAL(13, 4) NULL,
+  `currency`   VARCHAR(3)     NULL,
+  `category`   VARCHAR(50)    NULL,
+  `rating`     DECIMAL(4, 2)  NULL,
+  `url`        TEXT           NULL,
+  `created_on` DATETIME       NULL,
+  `updated_on` DATETIME       NULL,
+  `seller_id`  BIGINT         NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `aspire_shopee_transactions` (`id`),
   CONSTRAINT `aspire_shopee_transactions_seller_id` FOREIGN KEY (`seller_id`) REFERENCES `aspire_shopee_sellers` (`id`)
