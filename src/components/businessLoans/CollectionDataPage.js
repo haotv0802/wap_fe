9fad65ebc6ef9cf8582fe9608b7c6dcdbcd90970
Hao Ho ^ <hao.ho@wearefram.com>
2017-12-20 16:24:00 +0700

    Implement table displaying "crawled data".

--- src/components/businessLoans/CollectionDataPage.js
+++ src/components/businessLoans/CollectionDataPage.js
@@ -1,5 +1,4 @@
 import React, {PropTypes} from 'react';
-import {Link} from 'react-router';
 
 class CollectionDataPage extends React.Component {
 
@@ -9,10 +8,55 @@ class CollectionDataPage extends React.Component {
 
   render() {
     return (
-      <div className="panel panel-info">
-        <div className="panel-body"  >
-          <h1>Business Loans</h1>
-        </div>
+      <div className="panel panel-primary">
+          <div className="table-responsive">
+            <table className="table table-bordered table-hover" style={{width: "1200px"}}>
+              <thead style={{color: "#337AB7"}}>
+                <th className="col-sm-2">Name</th>
+                <th className="col-sm-1">Category</th>
+                <th className="col-sm-2">Vendor Name</th>
+                <th className="col-sm-1">Location</th>
+                <th className="col-sm-1">Ship on time</th>
+                <th className="col-sm-1">Positive</th>
+                <th className="col-sm-1">Neutral</th>
+                <th className="col-sm-1">Negative</th>
+                <th className="col-sm-1">Link</th>
+                <th className="col-sm-1">Time on lazada</th>
+                <th className="col-sm-1">Rating</th>
+                <th className="col-sm-1">Size</th>
+              </thead>
+              <tbody>
+              <tr>
+                <td>Name</td>
+                <td>Category</td>
+                <td>Vendor Name</td>
+                <td>Location</td>
+                <td>Shipping</td>
+                <td>Positive</td>
+                <td>Neutral</td>
+                <td>Negative</td>
+                <td>Link</td>
+                <td>Time on lazada</td>
+                <td>Rating</td>
+                <td>Size</td>
+              </tr>
+              <tr>
+                <td>Name</td>
+                <td>Category</td>
+                <td>Vendor Name</td>
+                <td>Location</td>
+                <td>Shipping</td>
+                <td>Positive</td>
+                <td>Neutral</td>
+                <td>Negative</td>
+                <td>Link</td>
+                <td>Time on lazada</td>
+                <td>Rating</td>
+                <td>Size</td>
+              </tr>
+              </tbody>
+            </table>
+          </div>
       </div>
     );
   }
