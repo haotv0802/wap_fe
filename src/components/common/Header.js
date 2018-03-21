7a39067d54a72b5c16c315488b9ce1f6d527c759
haoho <hoanhhao>
2018-03-21 13:59:39 +0700

    Clean up unused imports

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,21 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {Link, IndexLink} from 'react-router';
-import LoadingDots from './LoadingDots';
+import {Link} from 'react-router';
 import {connect} from 'react-redux';
 import * as loginActions from '../../actions/loginActions';
 import {bindActionCreators} from 'redux';
 import AppBar from 'material-ui/AppBar';
-import Paper from 'material-ui/Paper';
-import Menu from 'material-ui/Menu';
 import MenuItem from 'material-ui/MenuItem';
-import {Tabs, Tab} from 'material-ui/Tabs';
-import Slider from 'material-ui/Slider';
 import Drawer from 'material-ui/Drawer';
-import RaisedButton from 'material-ui/RaisedButton';
-import IconButton from 'material-ui/IconButton';
-import NavigationClose from 'material-ui/svg-icons/navigation/close';
-import FlatButton from 'material-ui/FlatButton';
 
 
 const styles = {
@@ -35,13 +26,9 @@ class Header extends React.Component {
       open: false
     };
     this.logout = this.logout.bind(this);
-    this.handleActive = this.handleActive.bind(this);
     this.handleToggle = this.handleToggle.bind(this);
     this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleActive(tab) {
-    this.context.router.push(tab.props['data-route']);
+    this.handleRequestChange = this.handleRequestChange.bind(this);
   }
 
   handleClick() {
@@ -49,7 +36,11 @@ class Header extends React.Component {
   }
 
   handleToggle() {
-    this.setState({open: !this.state.open})
+    this.setState({open: !this.state.open});
+  }
+
+  handleRequestChange() {
+    this.setState({open: !this.state.open});
   }
 
   logout(event) {
@@ -65,15 +56,15 @@ class Header extends React.Component {
           title={<span style={styles.title}>Title</span>}
           onTitleClick={this.handleClick}
           iconClassNameRight="muidocs-icon-navigation-expand-more"
-          iconElementLeft={<IconButton><NavigationClose /></IconButton>}
-          iconElementRight={<RaisedButton
-            label="Toggle Drawer"
-            onClick={this.handleToggle}
-          />}
+          onLeftIconButtonClick={this.handleToggle}
         />
-        <Drawer open={this.state.open}>
-          <MenuItem>Menu Item</MenuItem>
-          <MenuItem>Menu Item 2</MenuItem>
+        <Drawer
+          open={this.state.open}
+          onRequestChange={this.handleRequestChange}
+          docked={false}
+        >
+          <MenuItem containerElement={<Link to="/crawledData" />}>Crawled Data</MenuItem>
+          <MenuItem containerElement={<Link to="/crawling" />}>Crawling</MenuItem>
         </Drawer>
       </div>
     );
