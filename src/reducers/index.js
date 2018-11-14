299afeda8ea68b3c69cbc330c6a351476a7227d5
haoho <hoanhhao>
2018-11-14 16:04:31 +0700

    Implement postsCount

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,6 +2,7 @@ import {combineReducers} from 'redux';
 import credentials from './loginReducer';
 import crawledData from './crawledDataReducer';
 import contact from './contactReducer';
+import post from './postsReducer';
 import customer from './customerReducer';
 import crawlingInfo from './crawlingReducer';
 import signupUser from './signupReducer';
@@ -11,6 +12,7 @@ const rootReducer = combineReducers({
   credentials,
   signupUser,
   contact,
+  post,
   customer,
   crawledData,
   crawlingInfo,
